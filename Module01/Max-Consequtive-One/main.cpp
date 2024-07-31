2756b3f75066b3127b7eb27dade25dbce9263d7a
catnys <catnyseverdyn@gmail.com>
2024-07-31 21:33:57 +0300

    add soln to Max consequtive one

--- Module01/Max-Consequtive-One/main.cpp
+++ Module01/Max-Consequtive-One/main.cpp
@@ -8,11 +8,29 @@ using namespace std;
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxSequence = 0;
+
+        for(int num: nums) {
+            cout << "num : " << num << endl;
+            num == 1 ? maxSequence +=1 : maxSequence = 0;
+        }
         
+        return maxSequence;
     }
 };
 
 
 int main() {
-    cout << "hello" << endl;
+
+    int myArray[] = {1,1,0,1,1,1};
+    int size = sizeof(myArray) / sizeof(myArray[0]); // Calculate the size of the array
+
+
+    std::vector<int> someVector(myArray, myArray + size); // Convert array to vector
+    // vector<int> someVector  = {1,1,0,1,1,1};
+
+    Solution obj;
+    int result = obj.findMaxConsecutiveOnes(someVector);
+    cout << "Result: " << result << endl;
+
 }

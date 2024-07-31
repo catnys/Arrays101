073284ba2cbc61a7d698403fb45da8ce8ecd8beb
catnys <catnyseverdyn@gmail.com>
2024-07-31 17:10:02 +0300

    add max consequtive one

---
+++ Module01/Max-Consequtive-One/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+    }
+};
+
+
+int main() {
+    cout << "hello" << endl;
+}

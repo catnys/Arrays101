1a151da60abc76e849ca6bbd9ed2f9b8ecd62430
catnys <catnyseverdyn@gmail.com>
2024-08-01 16:38:27 +0300

    add squares of sorted array as solution

--- Module01/Squares-of-Sorted-Array/main.cpp
+++ Module01/Squares-of-Sorted-Array/main.cpp
@@ -8,20 +8,51 @@ using namespace std;
 class Solution {
 public:
     int sortedSquares(vector<int>& nums) {
+        display(nums);
 
-
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] *= nums[i];
+        }
+        display(nums);
+        bubbleSort(nums);
+        display(nums);
+        return 0;
     }
+
+    void bubbleSort(vector<int>& nums){
+        cout << nums.size() << endl;
+        for(int i = 0; i < nums.size(); i++) 
+            for(int j = 0; j < nums.size()-1; j++) {
+                if(nums[j] > nums[j+1])
+                    swap(&nums[j],&nums[j+1]);
+            }
+    }
+
+
+    void swap(int* a, int* b) {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    void display(vector<int> nums){
+        for(int num: nums) {
+            cout << "num: " << num << endl;
+        }
+    }
+
+
 };
 
 
 int main() {
 
-    int myArray[] = {-7,-3,2,3,11};
+    int myArray[] = {-4,-1,0,3,10};
     int size = sizeof(myArray) / sizeof(myArray[0]); // Calculate the size of the array
 
 
     std::vector<int> someVector(myArray, myArray + size); // Convert array to vector
-    // vector<int> someVector  = {-7,-3,2,3,11;
+    // vector<int> someVector  = {-4,-1,0,3,10;
 
     Solution obj;
     int result = obj.sortedSquares(someVector);

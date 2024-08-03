d88a5eebf368f2e3e3096b6df4d71dccfcce29d8
catnys <catnyseverdyn@gmail.com>
2024-08-03 13:12:10 +0300

    add zeros

--- Module02/DublicateZeros/main.cpp
+++ Module02/DublicateZeros/main.cpp
@@ -8,13 +8,11 @@ using namespace std;
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        for(size_t i = 0; i < arr.size(); i++) {
-            if(arr[i] == 0) {
-                arr.insert(arr.begin() + i + 1, 0);
-                // Skip the next element to avoid infinite loop on the newly inserted element
-                i++;
-            }
+        display(arr);
+        for(int i = arr.size() - 1; i >= 0; i--) {
+            arr[i + 1] = arr[i];
         }
+        display(arr);
     }
 
     void display(vector<int> arr) {

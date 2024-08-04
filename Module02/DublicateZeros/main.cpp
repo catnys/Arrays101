6397f4e16580d1a2b33959ef9ee2c88f90a19e76
catnys <catnyseverdyn@gmail.com>
2024-08-04 14:59:43 +0300

    add dublicate zeros

--- Module02/DublicateZeros/main.cpp
+++ Module02/DublicateZeros/main.cpp
@@ -8,10 +8,8 @@ using namespace std;
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        display(arr);
         for(int i = 0; i < arr.size(); i++) {
             if(arr[i] == 0){
-                cout << "alert !" << endl;
                 // iterate through that 0 and shift from that 0 till the end
                 for(int j = arr.size() - 1; j >= i+1; j--) {
                     arr[j+1] = arr[j];
@@ -19,7 +17,6 @@ public:
                 arr[i+1] = arr[i];
                 i++;
             }
-            display(arr);
         }
     }
 

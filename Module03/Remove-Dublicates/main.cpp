0c601fb6237389d5a236d07a58101d4711854b31
catnys <catnyseverdyn@gmail.com>
2024-08-05 23:07:35 +0300

    Complete remove dublicates

--- Module03/Remove-Dublicates/main.cpp
+++ Module03/Remove-Dublicates/main.cpp
@@ -7,14 +7,22 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int k = 1;
+
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+        return k;
     }
 };
 
 
 int main() {
 
-    int nums1[] = {0,1,2,2,3,0,4,2};
+    int nums1[] = {0,1,2,2,3,4};
 
     // Calculate the size of the array
     int m = sizeof(nums1) / sizeof(nums1[0]);
@@ -24,6 +32,10 @@ int main() {
 
     Solution obj;
     cout << obj.removeDuplicates(nums1Vect) << endl;
+
+    for(size_t i = 0; i < m; i++) {
+        cout << nums1Vect[i] << " ";
+    }
 
 
 }

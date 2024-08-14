1c3119570a6aa4249ad9f2eaae3f675b28e66afc
I'm just a cat <92661367+catnys@users.noreply.github.com>
2024-08-14 10:19:54 +0300

    Init main.cpp

--- Module04/ValidMountain/main.cpp
+++ Module04/ValidMountain/main.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
 
+using namespace std;
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        
+    }
+};
+
+
+
+
+int main() {
+
+    int myArray[] = {2,1};
+    int size = sizeof(myArray) / sizeof(myArray[0]); // Calculate the size of the array
+    vector<int> someVector(myArray, myArray + size); // Convert array to vector
+
+    Solution obj;
+    bool result = obj.checkIfExist(someVector);
+    cout << result << endl;
+
+}

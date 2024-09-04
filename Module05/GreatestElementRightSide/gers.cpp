eedbf04ca26b3109a02590a7f34a6b89b91afaf6
burak <burak@192.168.1.114>
2024-09-03 22:09:16 -0700

    add gers

---
+++ Module05/GreatestElementRightSide/gers.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        
+    }
+};
+
+
+
+
+int main() {
+
+    int myArray[] = {10,2,5,3};
+    int size = sizeof(myArray) / sizeof(myArray[0]); // Calculate the size of the array
+
+
+    vector<int> someVector(myArray, myArray + size); // Convert array to vector
+    // vector<int> someVector  = {-4,-1,0,3,10;
+
+    Solution obj;
+    vector<int> result = obj.replaceElements(someVector);
+
+}

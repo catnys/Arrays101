095c2d8eb22dd95b42366ec8167ca57523aaea60
I'm just a cat <92661367+catnys@users.noreply.github.com>
2024-09-30 10:47:47 +0300

    Create rdsa.cpp

---
+++ Module05/Remove-Dublicates-Sorted-Array/rdsa.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+    }
+};
+
+
+
+int main() {
+
+    int myvectay[] = {17,18,5,4,6,1};
+    int size = sizeof(myvectay) / sizeof(myvectay[0]); // Calculate the size of the vectay
+
+
+    vector<int> someVector(myvectay, myvectay + size); // Convert vectay to vector
+    cout << size << endl;
+
+    Solution obj;
+    vector<int> result = obj.replaceElements(someVector);
+
+}
